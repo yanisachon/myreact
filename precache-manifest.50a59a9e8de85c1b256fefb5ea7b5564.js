f58b87fadfaf9a586615a80e848cbbb2129bf95f
yanisachon <nisachon.piyasin@gmail.com>
2019-11-05 13:47:33 +0700

    Updates

---
+++ precache-manifest.50a59a9e8de85c1b256fefb5ea7b5564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2ee5ab108e39038bd1e6f04f02c803",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "12c9ea5538390e74bdb8",
+    "url": "/myreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d1410548a0945337b65e",
+    "url": "/myreact/static/js/2.595ad7a0.chunk.js"
+  },
+  {
+    "revision": "12c9ea5538390e74bdb8",
+    "url": "/myreact/static/js/main.88e5aa5e.chunk.js"
+  },
+  {
+    "revision": "f7257d6e7d64c553bbf4",
+    "url": "/myreact/static/js/runtime-main.2924f5c2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myreact/static/media/logo.25bf045c.svg"
+  }
+]);
